6d6e53267b67fd9044134e75a5a397a25b3d8ed8
CodeWith-Bilal <mb03086350364@gmail.com>
2024-09-22 14:44:23 +0500

    update root reducer

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -3,12 +3,18 @@ import userReducer from "../features/userSlice";
 import counterReducer from "../features/counterSlice";
 import todosReducer from "../features/todoSlice";
 import booksReducer from "../features/bookSlice";
+import postsReducer from "../features/postsSlice";
+import weatherReducer from "../features/weatherSlice";
+import githubReducer from "../features/githubSlice";
 
 const rootReducer = combineReducers({
   users: userReducer,
   counter: counterReducer,
   todos: todosReducer,
   books: booksReducer,
+  posts: postsReducer,
+  weather: weatherReducer,
+  github: githubReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
