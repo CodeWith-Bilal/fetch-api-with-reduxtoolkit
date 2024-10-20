b5201f0970178593d2a74f64c3a1aa1f018bfd11
CodeWith-Bilal <mb03086350364@gmail.com>
2024-10-20 14:59:54 +0500

    create book analytic page

---
+++ src/pages/AnalyticsPage.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../app/rootReducer';
+
+const AnalyticsPage: React.FC = () => {
+  const books = useSelector((s: RootState) => (s as any).books.items as any[]);
+  const totalBooks = books.reduce((sum, b) => sum + b.quantity, 0);
+  const inventoryValue = books.reduce((sum, b) => sum + b.price * b.quantity, 0);
+
+  return (
+    <div>
+      <h2>Analytics</h2>
+      <p>Total distinct titles: {books.length}</p>
+      <p>Total copies in stock: {totalBooks}</p>
+      <p>Total inventory value: ${inventoryValue.toFixed(2)}</p>
+    </div>
+  );
+};
+
+export default AnalyticsPage;
