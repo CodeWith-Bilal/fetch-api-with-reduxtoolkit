84ca46743c71cb655946bc463bc9881efc3b30e0
CodeWith-Bilal <mb03086350364@gmail.com>
2024-10-22 14:58:34 +0500

    create setting page

---
+++ src/pages/SettingsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SettingsPage: React.FC = () => {
+  return (
+    <div>
+      <h2>Settings</h2>
+      <p>Here you can configure simple demo settings (none implemented).</p>
+    </div>
+  );
+};
+
+export default SettingsPage;
