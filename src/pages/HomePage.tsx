9dcd9acf2b41728d85c6b2f5bbd9e2ee2238f4a8
CodeWith-Bilal <mb03086350364@gmail.com>
2024-11-10 14:43:20 +0500

    create the seperate home page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h2>Welcome</h2>
+      <p>This is the home page for the Redux Toolkit demo app.</p>
+    </div>
+  );
+};
+
+export default HomePage;
