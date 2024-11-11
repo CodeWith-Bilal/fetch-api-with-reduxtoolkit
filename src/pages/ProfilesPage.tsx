12f310c7960c3ee990aac5587d626019dff9d5ae
CodeWith-Bilal <mb03086350364@gmail.com>
2024-11-11 14:42:57 +0500

    create the seperate profile page

---
+++ src/pages/ProfilesPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProfileList from '../components/ProfileList';
+
+const ProfilesPage: React.FC = () => {
+  return (
+    <div>
+      <h2>Profiles</h2>
+      <ProfileList />
+    </div>
+  );
+};
+
+export default ProfilesPage;
