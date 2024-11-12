100672fba138c1aa5f9cc94b3ef47ae2c955887f
CodeWith-Bilal <mb03086350364@gmail.com>
2024-11-12 14:42:31 +0500

    create the seperate todopage

---
+++ src/pages/TodosPage.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import TodoInput from '../components/TodoInput';
+import TodoList from '../components/TodoList';
+
+const TodosPage: React.FC = () => {
+  return (
+    <div>
+      <h2>Todos</h2>
+      <div style={{ marginTop: 12 }}>
+        <TodoInput />
+      </div>
+      <div style={{ marginTop: 12 }}>
+        <TodoList />
+      </div>
+    </div>
+  );
+};
+
+export default TodosPage;
