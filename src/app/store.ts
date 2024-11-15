7884ccf81e0ae666c49bad83db51eeb0888a6510
CodeWith-Bilal <mb03086350364@gmail.com>
2024-11-15 14:35:26 +0500

    add the redux presist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,18 +1,31 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "../features/userSlice";
 import counterReducer from "../features/counterSlice";
 import todosReducer from "../features/todoSlice";
 import booksReducer from "../features/bookSlice";
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 
-const store = configureStore({
-  reducer: {
-    users: userReducer,
-    counter: counterReducer,
-    todos: todosReducer,
+const reducers = combineReducers({
+  users: userReducer,
+  counter: counterReducer,
+  todos: todosReducer,
   books: booksReducer,
-    // Add other reducers as needed
-  },
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['todos', 'books'], // persist only todos and books
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
