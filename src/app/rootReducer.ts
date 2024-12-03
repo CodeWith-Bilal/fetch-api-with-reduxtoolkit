6224045851f869f84eb34fec425f8f16f2233c77
CodeWith-Bilal <mb03086350364@gmail.com>
2024-12-03 13:07:31 +0500

    update rootreducer

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -2,10 +2,12 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import userReducer from '../features/userSlice';
 import counterReducer from '../features/counterSlice';
+import todosReducer from '../features/todoSlice';
 
 const rootReducer = combineReducers({
   users: userReducer,
   counter: counterReducer,
+  todos: todosReducer,
   // Add other reducers as needed
 });
 
