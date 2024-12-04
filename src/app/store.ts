d3133550a7b5f327e0b2ad103517e985183fc380
CodeWith-Bilal <mb03086350364@gmail.com>
2024-12-04 13:06:56 +0500

    update the store

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,11 +2,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../features/userSlice';
 import counterReducer from '../features/counterSlice';
+import todosReducer from '../features/todoSlice';
 
 const store = configureStore({
   reducer: {
     users: userReducer,
     counter: counterReducer,
+  todos: todosReducer,
     // Add other reducers as needed
   },
 });
