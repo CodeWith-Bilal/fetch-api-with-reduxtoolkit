944540bc3beda69d39131ba8551e5a8761634c90
CodeWith-Bilal <mb03086350364@gmail.com>
2024-12-26 12:44:19 +0500

    Add root reducer

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -2,9 +2,11 @@
 
 import { combineReducers } from '@reduxjs/toolkit';
 import userReducer from '../features/userSlice';
+import counterReducer from '../features/counterSlice';
 
 const rootReducer = combineReducers({
   users: userReducer,
+  counter: counterReducer,
   // Add other reducers as needed
 });
 
